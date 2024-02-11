32d0d868d4957ef96379dbe6c17d2f895f31c237
Space_Gamer <spacegamer1111@gmail.com>
2024-02-11 19:37:27 +0530

    Started parallel

--- src/parallel/main.cpp
+++ src/parallel/main.cpp
@@ -6,12 +6,22 @@
  * nc localhost <port> < input.txt
  */
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <cstring>
 #include <pthread.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unordered_map>
+#include <vector>
 
+using namespace std;
+
+int processClientRequest(int &);
+int createServerSocket(const int &);
 
 int main(int argc, char ** argv) {
   int portno; /* port to listen on */
@@ -27,4 +37,48 @@ int main(int argc, char ** argv) {
   // DONE: Server port number taken as command line argument
   portno = atoi(argv[1]);
 
+  // DONE: Create a server socket
+  int serverSocket = createServerSocket(portno);
+
+  if (listen(serverSocket, 5) < 0)
+  {
+    cerr << "Error: Unable to listen on socket" << endl;
+    close(serverSocket);
+    return -1;
+  }
+
+  cout << "Server listening on port: " << portno << endl;
+
+}
+
+int createServerSocket(const int & portno) {
+  int sockfd; /* socket */
+  struct sockaddr_in serveraddr; /* server's addr */
+  
+  /* 
+   * socket: create the parent socket 
+   */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("ERROR opening socket");
+    exit(1);
+  }
+  
+  /* 
+   * build the server's Internet address 
+   */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)portno);
+  
+  /* 
+   * bind: associate the parent socket with a port 
+   */
+  if (bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+    perror("ERROR on binding");
+    exit(1);
+  }
+  
+  return sockfd;
 }
